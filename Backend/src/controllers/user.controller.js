4fd6628d257bcd9738971861af80be8c6ab07140
amandeep <deepaman0556@gmail.com>
2025-09-08 23:43:19 +0530

    feat(backend-auth): add controller for jwt token generation

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,31 @@
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import User from "../models/user.model.js";
+import { ApiResponse } from "../utils/apiResponse.utils.js";
+import { ApiError } from "../utils/apiError.utils.js";
 
-const userSignup = asyncHandler((req, res) => {});
+const createAccessAndRefreshToken = asyncHandler(async (userId) => {
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new ApiError(404, "User does not exit");
+  }
+
+  try {
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    if (process.env.NODE_ENV === "development") {
+      console.error("Token generation error: ", error);
+    }
+    throw new ApiError(500, "Failed to generate access/refresh token");
+  }
+});
+const userSignup = asyncHandler((req, res) => {
+  const { fullname, email, password } = req.body;
+});
 const userLogin = asyncHandler((req, res) => {});
 const userLogout = asyncHandler((req, res) => {});
 
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/healthCheck", healthRouter);
 
