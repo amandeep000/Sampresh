403a29ccd09bf29636877334295bbfe0d5c0c123
amandeep <deepaman0556@gmail.com>
2025-09-09 09:59:48 +0530

    feat(backend-auth): add register user controller

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -23,8 +23,58 @@ const createAccessAndRefreshToken = asyncHandler(async (userId) => {
     throw new ApiError(500, "Failed to generate access/refresh token");
   }
 });
-const userSignup = asyncHandler((req, res) => {
+const userSignup = asyncHandler(async (req, res) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    throw new ApiError(400, "Req body cannot be empty!");
+  }
+
   const { fullname, email, password } = req.body;
+
+  if ([fullname, email, password].some((field) => !field?.trim())) {
+    throw new ApiError(
+      400,
+      "All the fields (fullname,email,password) are required."
+    );
+  }
+
+  const user = await User.findOne({ email });
+  if (user) {
+    return res
+      .status(400)
+      .json(new ApiResponse(400, "Email is already exists. Try login."));
+  }
+
+  const newUser = await User.create({
+    fullname,
+    email,
+    password,
+  });
+
+  if (!newUser) {
+    throw new ApiError(500, "Failed to register user. Try again later.");
+  }
+
+  const createdUser = {
+    id: newUser._id,
+    name: newUser.fullname,
+    email: newUser.email,
+  };
+
+  const { accessToken, refreshToken } = await createAccessAndRefreshToken(
+    newUser._id
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+  };
+
+  res
+    .status(201)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
 const userLogin = asyncHandler((req, res) => {});
 const userLogout = asyncHandler((req, res) => {});
