0e21c5a1cceff5ea8055b70c632afebf4c4272fa
amandeep <deepaman0556@gmail.com>
2025-10-04 23:02:09 +0530

    update(Frontend): update base url for production

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api/v1"
+      : "/api/v1",
   withCredentials: true,
 });
 
