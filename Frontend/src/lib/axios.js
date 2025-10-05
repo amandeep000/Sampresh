c387ceca1a290721b373c73ff9d6f792bac7e5e8
amandeep <deepaman0556@gmail.com>
2025-10-05 09:15:10 +0530

    update:Login loading state updated

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api/v1"
-      : "/api/v1",
+      : "https://sampresh.onrender.com/api/v1",
   withCredentials: true,
 });
 
